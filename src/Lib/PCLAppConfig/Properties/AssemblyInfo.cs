264e1397738770c4cd48557c54cfec6cd3e8b94b
Seyyed Soroosh Hosseinalipour <soorosh_abi@hotmail.com>
2016-07-19 00:34:56 +0430

    Add nuget packaging infrastructure.

--- src/Lib/PCLAppConfig/Properties/AssemblyInfo.cs
+++ src/Lib/PCLAppConfig/Properties/AssemblyInfo.cs
@@ -7,9 +7,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("PCLAppConfig")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Cross platform Xamarin forms App settings reader")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Ben Ishiyama-levy (Monovo), Seyyed Soroosh Hosseinalipour")]
 [assembly: AssemblyProduct("PCLAppConfig")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
@@ -26,5 +26,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0")]
+[assembly: AssemblyFileVersion("0.1.0")]
+[assembly: AssemblyInformationalVersion("0.1.0-alpha")]
