8698bcef464999a28c330304e6b037763304408e
Seyyed Soroosh Hosseinalipour <soorosh_abi@hotmail.com>
2022-07-08 16:02:27 +0430

    Resolve different location config loader in UWP. (fixes #30)

--- src/Lib/PCLAppConfig.FileSystemStream.UWP/UWPAppConfigPathExtractor.cs
+++ src/Lib/PCLAppConfig.FileSystemStream.UWP/UWPAppConfigPathExtractor.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,7 +15,15 @@ namespace PCLAppConfig.FileSystemStream
 			get
 			{
 				string rootPath = Package.Current.InstalledLocation.Path;
-				return System.IO.Path.Combine(rootPath, "App.config");
+				string exeConfig = System.IO.Path.Combine(rootPath, Package.Current.DisplayName + ".exe.config");
+				if (!File.Exists(exeConfig))
+				{
+					return System.IO.Path.Combine(rootPath, "App.config");
+				}
+				else
+				{
+					return exeConfig;
+				}
 			}
 		}
 	}
